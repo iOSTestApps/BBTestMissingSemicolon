755db5d60208b1bb9fe5e1a32c64ee82a3a81af5
Christopher Stott <christopherstott@gmail.com>
2015-05-04 09:16:14 -0700

    Initial Commit

---
+++ BBTestMissingSemicolon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBTestMissingSemicolon
+//
+//  Created by Chris on 2015-05-04.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBTestMissingSemicolon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BBTestMissingSemicolon
+//
+//  Created by Chris on 2015-05-04.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
